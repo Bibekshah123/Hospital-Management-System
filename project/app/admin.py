83ed1f86c46894deb96d03b2c1e15ada42800457
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2025-05-27 14:13:54 +0545

    .

--- project/app/admin.py
+++ project/app/admin.py
@@ -8,9 +8,7 @@ admin.site.register(Appointment)
 class DoctorAdmin(admin.ModelAdmin):
     list_display = ('name', 'specialization')
     search_fields = ('name', 'specialization')
-    prepopulated_fields = {'slug': ('name',)}
     
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('name', 'age', 'disease')
     search_fields = ('name', 'disease')
-    prepopulated_fields = {'slug': ('name',)}
